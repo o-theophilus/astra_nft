da26f19c6b2f8b523f303bec097281d9c24e6bf3
theophilus <theophilus.ogbolu@gmail.com>
2023-05-18 23:47:48 +0100

    remove redundant code

--- backend/application/api/nft.py
+++ backend/application/api/nft.py
@@ -68,28 +68,28 @@ def get_all():
 
 # unused
 
-@bp.get("/nft/<id>")
-def get(id):
+# @bp.get("/nft/<id>")
+# def get(id):
 
-    output = f"{getcwd()}/static"
+#     output = f"{getcwd()}/static"
 
-    with open(f"{output}/meta.json") as f:
-        data = json.load(f)
+#     with open(f"{output}/meta.json") as f:
+#         data = json.load(f)
 
-    meta = None
-    for i in data:
-        if i["id"] == int(id):
-            meta = i
-            break
+#     meta = None
+#     for i in data:
+#         if i["id"] == int(id):
+#             meta = i
+#             break
 
-    if not meta:
-        return jsonify({
-            "status": 401,
-            "message": "invalid request",
-        })
+#     if not meta:
+#         return jsonify({
+#             "status": 401,
+#             "message": "invalid request",
+#         })
 
-    return jsonify({
-        "status": 200,
-        "message": "ok",
-        "data": meta
-    })
+#     return jsonify({
+#         "status": 200,
+#         "message": "ok",
+#         "data": meta
+#     })
